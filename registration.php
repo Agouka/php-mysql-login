518c734a271e08f0f94c08109944e04ba409e44e
Aikal Ichsan <aikalichsan2@gmail.com>
2025-04-23 21:55:07 +0700

    Adding bootstrap and custom CSS

--- registration.php
+++ registration.php
@@ -1,29 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration Form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <div class="container">
         <form action="registration.php" method="post">
             <div class="form-group">
-                <input type="text" name="fullname" placeholder="Full Name:">
+                <input type="text" class="form-control" name="fullname" placeholder="Full Name:">
             </div>
             <div class="form-group">
-                <input type="email" name="email" placeholder="Email:">
+                <input type="email" class="form-control" name="email" placeholder="Email:">
             </div>
             <div class="form-group">
-                <input type="password" name="password" placeholder="Password:">
+                <input type="password" class="form-control" name="password" placeholder="Password:">
             </div>
             <div class="form-group">
-                <input type="text" name="repeat_password" placeholder="Repeat Password:">
+                <input type="text" class="form-control" name="repeat_password" placeholder="Repeat Password:">
             </div>
-            <div class="form-group">
-                <input type="Submit" value="Register" placeholder="submit">
+            <div class="form-btn">
+                <input type="Submit" class="btn btn-primary" value="Register" placeholder="submit">
             </div>
         </form>
     </div>
 </body>
+
 </html>
